b9e4220437d9c833c443cf42c2fa7f7a91f50dc8
bigchayanont <big.chayanont@gmail.com>
2021-01-08 22:27:51 +0700

    path fix

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,99 +1,109 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import Router from "vue-router";
 import Mainpage from "../views/Mainpage.vue";
-import Login from "../views/Login.vue";
-import Register from "../views/Register.vue";
-import Register1 from "@/components/Register1.vue";
-import Register2 from "@/components/Register2.vue"
-import Register3 from "@/components/Register3.vue";
-import Register4 from "@/components/Register4.vue";
-import Register5 from "@/components/Register5.vue";
-import profile from "../views/profile.vue";
-import MyAccount from "@/components/MyAccount.vue";
-import MyTicket from "@/components/MyTicket.vue";
-import souvenir from "../views/souvenir.vue";
-import newMainpage from "../views/newMainpage.vue"
-import backup from "@/components/backup.vue"
+const Login = () => import("../views/Login.vue");
+const Register = () => import("../views/Register.vue");
+const Register1 = () => import("@/components/Register1.vue");
+const Register2 = () => import("@/components/Register2.vue");
+const Register3 = () => import("@/components/Register3.vue");
+const Register4 = () => import("@/components/Register4.vue");
+const Register5 = () => import("@/components/Register5.vue");
+const profile = () => import("../views/profile.vue");
+const MyAccount = () => import("@/components/MyAccount.vue");
+const MyTicket = () => import("@/components/MyTicket.vue");
+const souvenir = () => import("../views/souvenir.vue");
+const newMainpage = () => import("../views/newMainpage.vue");
+const backup = () => import("@/components/backup.vue");
+const forget = () => import("@/components/forgetPass.vue");
+const resetpass = () => import("@/components/resetPass.vue");
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/mainpage",
-    name: "Mainpage",
-    component: Mainpage
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: Register
-  },
-  {
-    path: "/register1",
-    name: "Register1",
-    component: Register1
-  },
-  {
-    path: "/register2",
-    name: "Register2",
-    component: Register2
-  },
-  {
-    path: "/register3",
-    name: "Register3",
-    component: Register3
-  },
-  {
-    path: "/register4",
-    name: "Register4",
-    component: Register4
-  },
-  {
-    path: "/register5",
-    name: "Register5",
-    component: Register5
-  },
-  {
-    path: "/profile",
-    name: "profile",
-    component: profile
-  },
-  {
-    path: "/myaccount",
-    name: "myaccount",
-    component: MyAccount
-  },
-  {
-    path: "/myticket",
-    name: "myticket",
-    component: MyTicket
-  },
-  {
-    path: "/souvenir",
-    name: "souvenir",
-    component: souvenir
-  },
-  {
-    path: "/",
-    name: "newmain",
-    component: newMainpage
-  },
-  {
-    path: "/backup",
-    name: "backup",
-    component: backup
-  },
-];
-
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+const router = new Router({
+  // mode: "history",
+  // base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/mainpage",
+      name: "Mainpage",
+      component: Mainpage,
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: Register,
+    },
+    {
+      path: "/register1",
+      name: "Register1",
+      component: Register1,
+    },
+    {
+      path: "/register2",
+      name: "Register2",
+      component: Register2,
+    },
+    {
+      path: "/register3",
+      name: "Register3",
+      component: Register3,
+    },
+    {
+      path: "/register4",
+      name: "Register4",
+      component: Register4,
+    },
+    {
+      path: "/register5",
+      name: "Register5",
+      component: Register5,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: profile,
+    },
+    {
+      path: "/myaccount",
+      name: "myaccount",
+      component: MyAccount,
+    },
+    {
+      path: "/myticket",
+      name: "myticket",
+      component: MyTicket,
+    },
+    {
+      path: "/souvenir",
+      name: "souvenir",
+      component: souvenir,
+    },
+    {
+      path: "/",
+      name: "newmain",
+      component: newMainpage,
+    },
+    {
+      path: "/backup",
+      name: "backup",
+      component: backup,
+    },
+    {
+      path: "/forget",
+      name: "forget",
+      component: forget,
+    },
+    {
+      path: "/reset",
+      name: "reset",
+      component: resetpass,
+    },
+  ],
 });
 
 export default router;
