ab955d174254898dbc5a878d5e58d5b8330cace3
bigchayanont <big.chayanont@gmail.com>
2021-01-08 21:34:05 +0700

    modified for deployment

--- node/app/config/db.config.js
+++ node/app/config/db.config.js
@@ -1,8 +1,22 @@
+// module.exports = {
+//   HOST: process.env.DB_URL || "localhost",
+//   USER: "root",
+//   PASSWORD: "cpeNight!1",
+//   DB: "cpeNightdb",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
+
 module.exports = {
   HOST: process.env.DB_URL || "localhost",
-  USER: "root",
-  PASSWORD: "cpeNight!1",
-  DB: "cpeNightdb",
+  USER: "testuser",
+  PASSWORD: "P@ssw0rd",
+  DB: "testuser",
   dialect: "mysql",
   pool: {
     max: 5,
--- vue/src/http-common.js
+++ vue/src/http-common.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://10.26.100.190/api",
   headers: {
     "Content-type": "application/json",
-  }
+  },
 });
+
+
+// import axios from "axios";
+
+// export default axios.create({
+//   baseURL: "http://http://localhost:8080/api",
+//   headers: {
+//     "Content-type": "application/json",
+//   },
+// });
