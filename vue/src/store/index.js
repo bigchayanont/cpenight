ad227e5bae56cbdc2489c6aa79b97c942d78d34f
bigchayanont <big.chayanont@gmail.com>
2021-01-08 22:28:18 +0700

    initial value of variable to null

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -2,39 +2,40 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
-import { auth } from './auth.module';
-import { profile } from './user.module';
+import { auth } from "./auth.module";
+import { profile } from "./user.module";
 
 export default new Vuex.Store({
   state: {
-    userInfo:[
+    accountTypeCheck: "",
+    userInfo: [
       {
-      id: "",
-      email: null,
-      password : "",
-      accountType : "",
-      authId : "",
-      profilePic : "",
-      firstName: null,
-      lastName : null,
-      nickName: null,
-      birthday_day : "",
-      birthday_month : "",
-      birthday_year : "",
-      classOf : "",
-      phoneNumber : null,
-      line : "",
-      facebookAccount : "",
-      organ : "",
-      role : "",
-      field : "",
-      }
-    ]
+        id: null,
+        email: null,
+        password: null,
+        accountType: null,
+        authId: null,
+        profilePic: null,
+        firstName: null,
+        lastName: null,
+        nickName: null,
+        birthday_day: null,
+        birthday_month: null,
+        birthday_year: null,
+        classOf: null,
+        phoneNumber: null,
+        line: null,
+        facebookAccount: null,
+        organ: null,
+        role: null,
+        field: null,
+      },
+    ],
   },
   mutations: {},
   actions: {},
   modules: {
     auth,
-    profile
-  }
+    profile,
+  },
 });
